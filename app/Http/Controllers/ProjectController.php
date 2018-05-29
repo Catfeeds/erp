8cb95e18f89489cc57b6e9aab4cfe31df0641f22
DevZeng <zengshungeng@live.com>
2018-05-29 21:01:32 +0800

    fix

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -53,7 +53,8 @@ class ProjectController extends Controller
 //            'data'=>$data
 //        ]);
         if ($type){
-            if ($type=='buy_budgetary_edit'||$type=='buy_extrabugetary_edit'||$type=='build_finish_list'||$type=='stock_get_list'){
+            if ($type=='buy_budgetary_edit'||$type=='buy_extrabugetary_edit'||$type=='build_finish_list'||$type=='stock_get_list'||
+            $type=='loan_project_submit_edit'){
                 if ($number){
                     $DbObj->where('number','like','%'.$number.'%');
                 }
