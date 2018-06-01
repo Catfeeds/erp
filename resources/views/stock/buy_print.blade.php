23a1a8b8f167f7166b7cbe260f66f07fc01e3369
DevZeng <zengshungeng@live.com>
2018-06-01 18:05:41 +0800

    fix

--- resources/views/stock/buy_print.blade.php
+++ resources/views/stock/buy_print.blade.php
@@ -99,7 +99,7 @@
                 <th>{{$purchase->lists()->sum('number')}} ￥</th>
                 <th>{{number_format($purchase->lists()->sum('cost'),2)}} ￥</th>
                 <th>{{$buy_num}}</th>
-                <th>{{number_format($but_count,2)}}</th>
+                <th>{{number_format($buy_count,2)}}</th>
                 <th>{{$get_num}}</th>
                 <th>{{number_format($get_count,2)}}</th>
                 <th>{{$need_num}}</th>
