ba0f61bb9b18995eca4d40395a9d702afbf160ef
DevZeng <zengshungeng@live.com>
2018-06-01 22:19:04 +0800

    fix

--- public/js/pay_add.js
+++ public/js/pay_add.js
@@ -160,6 +160,9 @@
                     type: 'success'
                   })
                   $('.ui.dimmer').removeClass('active')
+                  setTimeout(() => {
+                      window.close();
+              }, 2000)
                 } else {
                   this.$notify({
                     title: '错误',
