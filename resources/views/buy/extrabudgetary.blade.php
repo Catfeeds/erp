e7228108c820179708f8eac8155958b7844244f3
ZENG <zeng@ZENGs-iMac.local>
2018-06-08 11:39:45 +0800

    fix

--- resources/views/buy/extrabudgetary.blade.php
+++ resources/views/buy/extrabudgetary.blade.php
@@ -118,7 +118,7 @@
                         <div class="inline fields">
                             <label class="four wide field">发票条件</label>
                             <div class="twelve wide field">
-                                <el-select v-model="extrabudgetary.info.content_id" placeholder="请选择内容">
+                                <el-select v-model="extrabudgetary.info.content" placeholder="请选择内容">
                                     <el-option v-for="item in invoiceType" :key="item.id" :label="item.name" :value="item.id">
                                     </el-option>
                                 </el-select>
