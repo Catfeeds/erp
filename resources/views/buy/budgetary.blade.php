c93d73cc038549866625f9f188ef5b5a291c7091
ZENG <zeng@ZENGs-iMac.local>
2018-06-27 11:52:01 +0800

    fix

--- resources/views/buy/budgetary.blade.php
+++ resources/views/buy/budgetary.blade.php
@@ -28,7 +28,7 @@
                                     </i>
                                     <template slot-scope="props">
                                         <div class="name" :title="props.item.name">@{{ props.item.number }}</div>
-                                        <span class="addr" title="@{{ props.item.name }}">@{{ props.item.name }}</span>
+                                        <span class="addr" :title="props.item.name">@{{ props.item.name }}</span>
                                     </template>
                                 </el-autocomplete>
                             </div>
@@ -43,8 +43,8 @@
                                     <i class="el-icon-edit el-input__icon" slot="suffix">
                                     </i>
                                     <template slot-scope="props">
-                                        <div class="name" title='@{{ props.item.name }}'>@{{ props.item.name }}</div>
-                                        <span class="addr" title='@{{  props.item.name }}'>@{{ props.item.number }}</span>
+                                        <div class="name" :title="props.item.name">@{{ props.item.name }}</div>
+                                        <span class="addr" :title="props.item.name">@{{ props.item.number }}</span>
                                     </template>
                                 </el-autocomplete>
                             </div>
