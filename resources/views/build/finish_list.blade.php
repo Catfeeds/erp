296265e046e653dd701f3cc35164eff5bf45ae12
DevZeng <zengshungeng@live.com>
2018-08-02 22:42:29 +0800

    fix;

--- resources/views/build/finish_list.blade.php
+++ resources/views/build/finish_list.blade.php
@@ -72,7 +72,7 @@
                     <td>{{$apply->team}}</td>
                     <td>{{$apply->manager}}</td>
                     <td>{{$apply->project_number}}</td>
-                    <td class="table-content" title="{{$apply->project_content}}">{{$apply->project->name}}</td>
+                    <td class="table-content" title="{{$apply->project->name}}">{{$apply->project->name}}</td>
                     <td>{{$apply->project->pm}}</td>
                     <td>￥{{number_format($apply->price,2)}} </td>
                     <td>{{$apply->applier}}</td>
